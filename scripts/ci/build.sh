544b41888b21f0718a5fde6bad3ea16f0035d3ea
tyxchen <tyxchen@outlook.com>
2017-07-16 22:11:15 -0400

    Add production hack to build script for _config.yml

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -5,11 +5,22 @@ set -e
 
 echo "Starting build"
 
+# Hackishly modify _config.yml for production
+# Since we can't change values based on environment, we sed the config instead
+# This replaces all key: value pairs flagged with a `prod: <prod value>'
+# comment
+# e.g.
+#   output: true # prod: false
+# becomes
+#   output: false
+sed -Ee 's/(.+:\s*)(.+)#\s*prod\s*:\s*(.+)/\1\3/' _config.yml > _config.yml
+
 # Build site
 bundle exec jekyll build
 
 # Delete and move files
-find . -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name '_site' ! -name 'circle.yml' -delete
+find . -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name '_site' \
+    ! -name 'circle.yml' -delete
 
 mv _site/* .
 rm -R _site/
