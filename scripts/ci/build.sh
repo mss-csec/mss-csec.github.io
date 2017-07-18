ff2ab321d7f1a5bccb856b7c8be1cbfb19fb2fd3
tyxchen <tyxchen@outlook.com>
2017-07-18 11:38:50 -0400

    Add normalizing file structure to build script

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -5,18 +5,25 @@ set -e
 
 echo "Starting build"
 
-# Hackishly modify _config.yml for production
-# Since we can't change values based on environment, we sed the config instead
-# This replaces all key: value pairs flagged with a `prod: <prod value>'
-# comment
-# e.g.
-#   output: true # prod: false
-# becomes
-#   output: false
-sed -Ee 's/(.+:\s*)(.+)#\s*prod\s*:\s*(.+)/\1\3/' _config.yml > _config-prod.yml
+# Normalize file structure
+# /_subclubs/:subclub/lessons/:lesson/:permatitle.md
+#   --> /_lessons/:subclub/:lesson-:permatitle.md
+for orig_path in `ls _subclubs/**/lessons/**/*.md`; do
+  subclub=`echo $orig_path | cut -d'/' -f2`
+  week=`   echo $orig_path | cut -d'/' -f4`
+  title=`  echo $orig_path | cut -d'/' -f5`
+
+  if [ ! -d "_lessons" ]; then mkdir "_lessons"; fi
+  if [ ! -d "_lessons/${subclub}" ]; then mkdir "_lessons/$subclub"; fi
+
+  cp $orig_path "_lessons/$subclub/$week-$title"
+done
+# /_subclubs/:subclub/resources
+#   --> /_resources/:subclub/
+# LOW PRIORITY
 
 # Build site
-bundle exec jekyll build --config _config-prod.yml
+bundle exec jekyll build --config _config.yml,_config-prod.yml
 
 # Delete and move files
 find . -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name '_site' \
