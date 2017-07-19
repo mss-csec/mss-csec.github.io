05361c0cf64904f896e540dcb8c8d27b97066d27
MSS CSEC <markville.compsci@gmail.com>
2017-07-19 20:22:45 +0000

    Deploy 83af0b3

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+/*
+Main coffeescript file
+ */
+
+(function() {
+  var onload;
+
+  onload = function() {
+    return console.log("Testing Coffeescript");
+  };
+
+  $(onload);
+
+}).call(this);
