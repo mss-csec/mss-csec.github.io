1167c282a388f4180a05864c7d6af9210bca2346
tyxchen <tyxchen@outlook.com>
2017-07-28 21:57:43 -0400

    Patch jekyll-asciidoc in CI setup

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -16,6 +16,16 @@ gem install bundler --conservative
 bundle check || bundle install
 bundle update
 
+# Patch Jekyll Asciidoc
+# There's an issue where defining the category property on a page means that
+# that page loses its category property
+# See https://github.com/asciidoctor/jekyll-asciidoc/pull/160 for patch
+jekyll_asciidoc_path=`bundle show "jekyll-asciidoc"`
+if [ `echo $jekyll_asciidoc_path | grep '[0-2]\(\.[0-9]\+\)\+$'` ]; then
+  jekyll_asciidoc_path=jekyll_asciidoc_path/lib/jekyll-asciidoc/integrator.rb
+  sed -i.bak "s/data\.delete sole_key/data[sole_key]/" $jekyll_asciidoc_path
+fi
+
 echo "Setup successful"
 
 exit 0
