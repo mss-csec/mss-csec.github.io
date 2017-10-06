cc503df327d42563b47b66bef5dc437a228500ad
MSS CSEC <markville.compsci@gmail.com>
2017-10-06 18:05:49 +0000

    Deploy 8e41095

--- assets/js/search.js
+++ assets/js/search.js
@@ -38,7 +38,7 @@
 
   initSearch = function(rawStore) {
     var commit, savedStore;
-    commit = "58f3c30";
+    commit = "8e41095";
     if (null !== localStorage.getItem(storageKey)) {
       try {
         savedStore = JSON.parse(localStorage.getItem(storageKey));
