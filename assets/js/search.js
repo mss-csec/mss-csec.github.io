0c371ce933623b9dd05854e683176f730ffc17e9
MSS CSEC <markville.compsci@gmail.com>
2017-10-24 04:03:17 +0000

    Deploy dbf6988

--- assets/js/search.js
+++ assets/js/search.js
@@ -42,7 +42,7 @@
 
   initSearch = function(rawStore) {
     var commit, savedStore;
-    commit = "71f94bc";
+    commit = "dbf6988";
     if (null !== localStorage.getItem(storageKey)) {
       try {
         savedStore = JSON.parse(localStorage.getItem(storageKey));
