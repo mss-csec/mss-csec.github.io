6452b4753062c93a7b33d118e102c5a38882a01c
MSS CSEC <markville.compsci@gmail.com>
2018-01-04 17:18:50 +0000

    Deploy 8ad95249add6f95c84751d18267911342a0be152

--- assets/js/consts.js
+++
@@ -1,1 +0,0 @@
-(function(){window.CONSTS={ex:20,bpMobile:400,bpPhablet:550,bpTablet:768,bpDesktop:1024,bpDesktopHd:1440,animDuration:230,cookieCollapseSidebar:"collapseSidebar",cookieStickyPrefix:"sticky-",cookieTheme:"theme",easterEgg:"pascha"},Object.freeze(window.CONSTS)}).call(this);
